eb6af1da5ba2f3fff66fd64b2e0dbdfe70b72f4d
SoapyHoang <sxhoang@ucsd.edu>
2022-03-09 01:56:51 -0800

    removed extra let

--- js/01-coinflip-redux.js
+++ js/01-coinflip-redux.js
@@ -2,7 +2,7 @@
  let amount = parseFloat(prompt('How many times would you like to flip a coin?'));
 
  for (let i= 1; i <= amount; i++) {
-    let coinFlip = Math.round(Math.random());
+    coinFlip = Math.round(Math.random());
     if (coinFlip == 0) {
         console.log('Heads')
     } else if (coinFlip == 1) {
